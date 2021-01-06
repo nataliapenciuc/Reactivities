824007f1239fa94e45e3e903cba3385961a99bef
natalia <natalia.smintanca@gmail.com>
2021-01-06 13:42:56 +0200

    add client

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,7 +30,11 @@ namespace API
             services.AddDbContext<DataContext>(opt=>{
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
-
+        services.AddCors(opt=>{
+            opt.AddPolicy("CorsPolicy", policy =>{
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+            });
+        });
             services.AddControllers();
         }
 
@@ -45,9 +49,9 @@ namespace API
            // app.UseHttpsRedirection();
 
             app.UseRouting(); 
-
+            
             app.UseAuthorization();
-
+            app.UseCors("CorsPolicy");
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
